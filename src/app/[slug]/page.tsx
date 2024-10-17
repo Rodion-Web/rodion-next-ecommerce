fa930e9b589964835ab91fc3db8057f83639ac3f
Rodion-Web <rodeus770@gmail.com>
2024-10-17 21:30:21 +0300

    changed

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,61 @@
-const SinglePage = () => {
-    return (
-      <div className=''>SinglePage</div>
-    )
-  }
-  
-  export default SinglePage
+import Add from "@/components/Add";
+import CustomizeProducts from "@/components/CustomizeProducts";
+import ProductImages from "@/components/ProductImages";
 
- 
+const SinglePage = () => {
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* IMG */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
+      {/* TEXTS */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          ipsam voluptatum omnis sapiente quia cumque modi placeat repellat
+          distinctio magnam eaque incidunt asperiores praesentium quidem
+          reprehenderit, error adipisci eos laborum!
+        </p>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="flex item-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">$59</h3>
+          <h2 className="font-medium text-2xl">$49</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100" />
+        <CustomizeProducts />
+        <Add />
+        <div className="text-sm ">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p className="">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+            ex? Autem, ducimus sit quis consectetur voluptate illo hic quae
+            tempora dolores, laudantium harum. Quibusdam dolore impedit eius
+            officia modi neque.
+          </p>
+        </div>
+        <div className="text-sm ">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p className="">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+            ex? Autem, ducimus sit quis consectetur voluptate illo hic quae
+            tempora dolores, laudantium harum. Quibusdam dolore impedit eius
+            officia modi neque.
+          </p>
+        </div>
+        <div className="text-sm ">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p className="">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+            ex? Autem, ducimus sit quis consectetur voluptate illo hic quae
+            tempora dolores, laudantium harum. Quibusdam dolore impedit eius
+            officia modi neque.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
-const HomePage = () => {
-  return (
-    <div className=''>HomePage</div>
-  )
-}
+"use client";
 
-export default HomePage
+import CategoryList from "@/components/CategoryList";
+import ProductList from "@/components/ProductList";
+import { WixClientContext } from "@/context/wixContext";
+import { useContext, useEffect } from "react";
+
+const HomePage = () => {
+  const wixClient = useContext(WixClientContext);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await wixClient.products.queryProducts().find();
+
+      console.log(res);
+    };
+
+    getProducts();
+  }, [wixClient]);
+
+  return (
+    <div className="">
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">Featured Products</h1>
+        <ProductList />
+      </div>
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Categories
+        </h1>
+        <CategoryList />
+      </div>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">New Products</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
